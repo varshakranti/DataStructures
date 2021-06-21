16b4083d8550138211adb90a253c152d4c6d777c
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-06-21 19:49:04 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    // int *aptr ;
+    // aptr = &a;
+
+    int *aptr = &a;
+
+    cout<<*aptr<<endl;
+    *aptr = 20;
+    cout << a <<endl;
+    
+
+    return 0;
+}
