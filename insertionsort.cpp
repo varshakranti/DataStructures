e0172f0137b022e58ab2a5e183950422366f0bf0
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-06-23 10:34:49 +0530

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of your array"<<endl;
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements of your array"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=1; i<n; i++)
+    {
+        int current = arr[i];
+        int j=i-1;
+        while(arr[j]>current && j>=0)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+
+    cout<<"Sorted array is"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
