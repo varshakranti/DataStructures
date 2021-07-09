b424e7320fa7b75227eabbf2fce257740b45e5b9
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-07-09 15:30:51 +0530

    Add files via upload

---
+++ balancedParenthesis.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isvalid(string s){
+    int n = s.size();
+    stack<char> st;
+
+    bool ans = true;
+    for(int i=0; i<n; i++){
+        if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if(s[i] == ')')
+        {
+            if(!st.empty() && st.top()=='(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if(!st.empty() && s[i] == '}')
+        {
+            if(st.top()=='{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if(!st.empty() && s[i] == ']')
+        {
+            if(st.top()=='[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    if(!st.empty())
+    {
+        return false;
+    }
+
+    return ans;
+
+}
+
+int main(){
+    string s = "{([])}";
+    if(isvalid(s)){
+        cout<<"Valid String"<<endl;
+    }
+    else{
+        cout<<"Invalid String"<<endl;
+    }
+    return 0;
+}
