e1f18d4a3f3b8345d9ffe0747a9e91e4196c1eb8
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-08-30 19:16:42 +0530

    Update buildTreeFromInorderPreorder.cpp

--- Tree/buildTreeFromInorderPreorder.cpp
+++ Tree/buildTreeFromInorderPreorder.cpp
@@ -1,1 +1,70 @@
+//Pick element from preorder & create a node
+//Increment preorder idx
+//Search for picked element's pos in inorder
+//Call to build left subtree from inorder's 0 to pos-1
+//Call to build right subtree from inorder pos+1 to n
+//Return the node
+
+#include "bits/stdc++.h"
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int search(int inorder[],int start, int end, int curr){
+    for(int i=start; i<=end; i++){
+        if(inorder[i] == curr){
+            return i;
+        }
+    }
+    return -1;
+}
+
+Node* buildTree(int preorder[], int inorder[], int start, int end){
+    static int idx = 0;
+    if(start > end){
+        return NULL;
+    }
+    int curr = preorder[idx];
+    idx++;
+    Node * node = new Node(curr);
+    if(start == end){
+        return node;
+    }
+    int pos = search(inorder,start,end,curr);
+    node->left = buildTree(preorder,inorder,start,pos-1);
+    node->right = buildTree(preorder, inorder, pos+1, end);
+
+    return node;
+}
+
+void inorderPrint(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorderPrint(root->left);
+    cout<<root->data<<" ";
+    inorderPrint(root->right);
+}
+
+int main(){
+    int preorder[] = {1, 2, 4, 3, 5};
+    int inorder[] = {4, 2, 1, 5, 3};
+
+    //buildTree
+    Node* root = buildTree(preorder, inorder, 0, 4);
+    inorderPrint(root);
+
+
+    return 0;
+}
 
