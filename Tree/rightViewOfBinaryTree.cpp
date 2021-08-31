59b9af72eaf9462fe53be4a2fa768a7a0f99d337
Varsha R Kranti <75674655+varshakranti@users.noreply.github.com>
2021-08-31 20:55:54 +0530

    Update rightViewOfBinaryTree.cpp

--- Tree/rightViewOfBinaryTree.cpp
+++ Tree/rightViewOfBinaryTree.cpp
@@ -13,7 +13,7 @@ struct Node{
     }
 };
 
-void leftView(Node* root){
+void rightView(Node* root){
     if(root == NULL){
         return;
     }
@@ -27,7 +27,7 @@ void leftView(Node* root){
             Node* curr = q.front();
             q.pop();
 
-            if(i == 0){
+            if(i == n-1){
                 cout<<curr->data<<" ";
             }
             if(curr->left != NULL){
@@ -40,16 +40,16 @@ void leftView(Node* root){
     }
 }
 int main(){
-    struct Node* root = new Node(5);
-    root->left = new Node(3);
-    root->right = new Node(6);
-    root->left->left = new Node(2);
-    root->left->right = new Node(4);
-    // root->right->left = new Node(6);
-    // root->right->right = new Node(7);
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 
-    //left view
-    leftView(root);
+    //right view
+    rightView(root);
 
     return 0;
 }
